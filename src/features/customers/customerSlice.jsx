d38e4d71c7c14b83828ef4ca6c26cbf5a0e6c31d
Benedict <aliubenedict@gmail.com>
2023-12-31 19:40:35 +0100

    first commit upload to github

---
+++ src/features/customers/customerSlice.jsx
@@ -0,0 +1,34 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+const customerReducer = (state = initialStateCustomer, action) => {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const createCustomer = (fullName, nationalID) => {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+};
+export const updateName = (fullName) => {
+  return { type: "account/updateName", payload: fullName };
+};
+// store.dispatch(createCustomer("Benedict", "ENG1708899"));
+// console.log(store.getState());
+export default customerReducer;
